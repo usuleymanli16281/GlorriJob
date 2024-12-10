5cfe405f41005cf73a3dda3af34f09bf3f74295e
Umid <suleymanli4240@gmail.com>
2024-12-10 18:58:40 +0400

    Modified folders

--- src/Presentation/GlorriJob.WebAPI/Program.cs
+++ src/Presentation/GlorriJob.WebAPI/Program.cs
@@ -9,9 +9,7 @@ builder.Services.AddPersistentServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
