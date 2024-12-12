1b275f8aaf5bfc7bb40578ca6202729dce342d56
VusalaAlili <vusalarg@gmail.com>
2024-12-12 14:10:43 +0400

    Added ICityService and GetCityDto,CreateCityDto

---
+++ src/Core/GlorriJob.Application/Abstractions/Services/ICityService.cs
@@ -0,0 +1,15 @@
+﻿using GlorriJob.Application.Dtos;
+using GlorriJob.Domain.Shared;
+
+namespace GlorriJob.Application.Abstractions.Services;
+
+public interface ICityService
+{
+    Task<GetCityDto> GetByIdAsync(Guid id);
+    Task<Pagination<GetCityDto>> GetAll(int pageNumber = 1, int take = 10, bool isPaginated = false);
+    Task<Pagination<GetCityDto>> SearchByName(string name, int pageNumber = 1, int take = 10, bool isPaginated = false);
+    Task<GetCityDto> CreateAsync(CreateCityDto createCityDto);
+    Task<GetCityDto> UpdateAsync(CreateCityDto updateCityDto);
+    Task DeleteAsync(Guid id);
+
+}
