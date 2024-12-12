68242ae038c905ece37445dec6cb7b69b766a97e
Umid <suleymanli4240@gmail.com>
2024-12-12 10:30:39 +0400

    Added pagination in domain layer

---
+++ src/Core/GlorriJob.Domain/Entities/Common/Pagination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlorriJob.Domain.Entities.Common
+{
+	public class Pagination<T>
+	{
+		public List<T> Items { get; set; } = null!;
+		public int TotalCount { get; set; }
+		public int PageIndex { get; set; }
+		public int PageSize { get; set; }
+	}
+}
