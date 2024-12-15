dc6f3cf1ab7dec72a9bf6e2c6422c79a8f8b094d
Umid <suleymanli4240@gmail.com>
2024-12-15 13:02:19 +0400

    Added Department Repository

---
+++ src/Core/GlorriJob.Application/Abstractions/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using GlorriJob.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlorriJob.Application.Abstractions.Repositories
+{
+	public interface IDepartmentRepository : IRepository<Department>
+	{
+	}
+}
