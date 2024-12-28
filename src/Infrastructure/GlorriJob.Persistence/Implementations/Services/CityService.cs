c3897800b333a56431e9daf660e382a8bd04e8a7
URLeeo <mammadzadaaslan05@gmail.com>
2024-12-28 22:26:17 +0400

    glorrijobs-21/modified UpdateAsync in CityService

--- src/Infrastructure/GlorriJob.Persistence/Implementations/Services/CityService.cs
+++ src/Infrastructure/GlorriJob.Persistence/Implementations/Services/CityService.cs
@@ -23,14 +23,14 @@ internal class CityService : ICityService
     }
     public async Task<CityGetDto> CreateAsync(CityCreateDto cityCreateDto)
     {
-		var validator = new CityCreateValidator();
-		var validationResult = await validator.ValidateAsync(cityCreateDto);
+        var validator = new CityCreateValidator();
+        var validationResult = await validator.ValidateAsync(cityCreateDto);
 
-		if (!validationResult.IsValid)
-		{
-			throw new ValidationException(validationResult.Errors);
-		}
-		var existedCity = await _cityRepository.GetByFilter(expression:
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        var existedCity = await _cityRepository.GetByFilter(expression:
             c => c.Name == cityCreateDto.Name && !c.IsDeleted,
             isTracking: false);
 
@@ -55,30 +55,30 @@ internal class CityService : ICityService
         await _cityRepository.SaveChangesAsync();
     }
 
-	public async Task<Pagination<CityGetDto>> GetAllAsync(int pageNumber = 1, int pageSize = 10, bool isPaginated = false)
-	{
-		if (pageNumber < 1 || pageSize < 1)
-			throw new BadRequestException("Page number and page size should be greater than 0.");
+    public async Task<Pagination<CityGetDto>> GetAllAsync(int pageNumber = 1, int pageSize = 10, bool isPaginated = false)
+    {
+        if (pageNumber < 1 || pageSize < 1)
+            throw new BadRequestException("Page number and page size should be greater than 0.");
 
-		IQueryable<City> query = _cityRepository.GetAll(c => !c.IsDeleted);
+        IQueryable<City> query = _cityRepository.GetAll(c => !c.IsDeleted);
         int totalItems = await query.CountAsync();
         if (isPaginated)
         {
             int skip = (pageNumber - 1) * pageSize;
             query = query.Skip(skip).Take(pageSize);
         }
-		List<CityGetDto> cityGetDtos = await query.Select(c => new CityGetDto { Id = c.Id, Name = c.Name}).ToListAsync();
-		return new Pagination<CityGetDto>
-		{
-			Items = cityGetDtos,
-			TotalCount = totalItems,
-			PageIndex = pageNumber,
-			PageSize = isPaginated ? pageSize : totalItems,
+        List<CityGetDto> cityGetDtos = await query.Select(c => new CityGetDto { Id = c.Id, Name = c.Name }).ToListAsync();
+        return new Pagination<CityGetDto>
+        {
+            Items = cityGetDtos,
+            TotalCount = totalItems,
+            PageIndex = pageNumber,
+            PageSize = isPaginated ? pageSize : totalItems,
             TotalPage = (int)Math.Ceiling((double)totalItems / pageSize),
-		};
-	}
+        };
+    }
 
-	public async Task<CityGetDto> GetByIdAsync(Guid id)
+    public async Task<CityGetDto> GetByIdAsync(Guid id)
     {
         var city = await _cityRepository.GetByIdAsync(id);
         if (city is null || city.IsDeleted)
@@ -92,48 +92,48 @@ internal class CityService : ICityService
 
     public async Task<Pagination<CityGetDto>> SearchByNameAsync(string name, int pageNumber = 1, int pageSize = 10, bool isPaginated = false)
     {
-		if (pageNumber < 1 || pageSize < 1)
-			throw new BadRequestException("Page number and page size should be greater than 0.");
+        if (pageNumber < 1 || pageSize < 1)
+            throw new BadRequestException("Page number and page size should be greater than 0.");
 
-		IQueryable<City> query = _cityRepository.GetAll(c => !c.IsDeleted && c.Name.ToLower().Contains(name.ToLower()));
-		int totalItem = await query.CountAsync();
-		if (isPaginated)
-		{
-			int skip = (pageNumber - 1) * pageSize;
-			query = query.Skip(skip).Take(pageSize);
-		}
-		List<CityGetDto> cityGetDtos = await query.Select(c => new CityGetDto { Id = c.Id, Name = c.Name }).ToListAsync();
-		return new Pagination<CityGetDto>
-		{
-			Items = cityGetDtos,
-			TotalCount = totalItem,
-			PageIndex = pageNumber,
-			PageSize = isPaginated ? pageSize : totalItem,
-			TotalPage = (int)Math.Ceiling((double)totalItem / pageSize),
-		};
-	}
+        IQueryable<City> query = _cityRepository.GetAll(c => !c.IsDeleted && c.Name.ToLower().Contains(name.ToLower()));
+        int totalItem = await query.CountAsync();
+        if (isPaginated)
+        {
+            int skip = (pageNumber - 1) * pageSize;
+            query = query.Skip(skip).Take(pageSize);
+        }
+        List<CityGetDto> cityGetDtos = await query.Select(c => new CityGetDto { Id = c.Id, Name = c.Name }).ToListAsync();
+        return new Pagination<CityGetDto>
+        {
+            Items = cityGetDtos,
+            TotalCount = totalItem,
+            PageIndex = pageNumber,
+            PageSize = isPaginated ? pageSize : totalItem,
+            TotalPage = (int)Math.Ceiling((double)totalItem / pageSize),
+        };
+    }
 
     public async Task<CityUpdateDto> UpdateAsync(Guid id, CityUpdateDto cityUpdateDto)
     {
-        if(id != cityUpdateDto.Id)
+        if (id != cityUpdateDto.Id)
         {
             throw new BadRequestException("Id doesn't match with the root");
         }
-		var validator = new CityUpdateValidator();
-		var validationResult = await validator.ValidateAsync(cityUpdateDto);
-
-		if (!validationResult.IsValid)
-		{
-			throw new ValidationException(validationResult.Errors);
-		}
-		var city = await _cityRepository.GetByIdAsync(id);
+        var validator = new CityUpdateValidator();
+        var validationResult = await validator.ValidateAsync(cityUpdateDto);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        var city = await _cityRepository.GetByIdAsync(id);
         if (city is null || city.IsDeleted)
         {
             throw new NotFoundException("This city does not exist.");
         }
-        var modifiedCity = _mapper.Map<City>(cityUpdateDto);
-        _cityRepository.Update(modifiedCity);
+        _mapper.Map(cityUpdateDto, city);
+        _cityRepository.Update(city);
         await _cityRepository.SaveChangesAsync();
         return cityUpdateDto;
     }
+
 }
